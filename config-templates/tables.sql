654b9cf918c648d97f709f657762e82026045774
Pavel Vyskocil <vyskocilpavel@muni.cz>
2017-09-01 10:00:24 +0200

    Added sql script for create tables

---
+++ config-templates/tables.sql
@@ -0,0 +1,27 @@
+#Statistics for proxy
+CREATE TABLE statistics_proxy (
+	year INT NOT NULL,
+	month INT NOT NULL,
+	day INT NOT NULL,
+	sourceIdp VARCHAR(255) NOT NULL,
+	count INT,
+	INDEX (sourceIdp),
+	INDEX (year),
+	INDEX (year,month),
+	INDEX (year,month,day),
+	PRIMARY KEY (year, month, day, sourceIdp)
+);
+
+#Statistics for services
+CREATE TABLE statistics_services( 
+	year INT NOT NULL,
+	month INT NOT NULL,
+	day INT NOT NULL,
+	service VARCHAR(255) NOT NULL,
+	count INT,
+	INDEX (service),
+	INDEX (year),
+	INDEX (year,month),
+	INDEX (year,month,day),
+	PRIMARY KEY (year, month, day, service)
+);
