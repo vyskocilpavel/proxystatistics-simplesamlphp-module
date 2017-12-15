50c49e300ea56ee240a55b12cdd76b13311c2c22
Pavel Vyskocil <vyskocilpavel@muni.cz>
2017-12-15 15:17:05 +0100

    Added support for SSL
    * Added option to set port
    * Added support for use SSL to connection to DB

--- config-templates/module_statisticsproxy.php
+++ config-templates/module_statisticsproxy.php
@@ -8,15 +8,72 @@
 
 $config = array(
 
+    /*
+     * Fill the serverName
+     */
     'serverName' => 'localhost',
 
+    /*
+     * If you want to use the default port, please comment option 'port'
+     */
+    'port' => 3306,
+
+    /*
+     * Fill the user name
+     */
     'userName' => 'stats',
 
+    /*
+     * Fill the password
+     */
     'password' => 'stats',
 
+    /*
+     * Fill the database name
+     */
     'databaseName' => 'STATS',
 
+    /*
+     * Fill the table name for identityProviders
+     */
     'identityProvidersTableName' => 'identityProviders',
 
+    /*
+     * Fill the table name for serviceProviders
+     */
     'serviceProvidersTableName' => 'serviceProviders',
+
+    /*
+     * Fill true, if you want to use encryption, false if not.
+     */
+    'encryption' => true/false,
+
+    /*
+     * The path name to the certificate authority file.
+     *
+     * If you use encryption, you must fill this option.
+     */
+    'ssl_ca' => '/example/ca.pem',
+
+    /*
+     * The path name to the certificate file.
+     *
+     * If you use encryption, you must fill this option.
+     */
+    'ssl_cert_path' => '/example/cert.pem',
+
+    /*
+     * The path name to the key file.
+     *
+     * If you use encryption, you must fill this option.
+     */
+    'ssl_key_path' => '/example/key.pem',
+
+    /*
+     * The pathname to a directory that contains trusted SSL CA certificates in PEM format.
+     *
+     * If you use encryption, you must fill this option.
+     */
+    'ssl_ca_path' => '/etc/ssl',
+
 );
--- lib/Auth/Process/DatabaseConnector.php
+++ lib/Auth/Process/DatabaseConnector.php
@@ -6,19 +6,31 @@
 class databaseConnector
 {
     private $serverName;
+    private $port;
     private $username;
     private $password;
     private $databaseName;
     private $identityProvidersTableName;
     private $serviceProvidersTableName;
+    private $encryption;
+    private $sslCA;
+    private $sslCert;
+    private $sslKey;
+    private $sslCAPath;
 
     const CONFIG_FILE_NAME = 'module_statisticsproxy.php';
     const SERVER  = 'serverName';
+    const PORT = 'port';
     const USER = 'userName';
     const PASSWORD = 'password';
     const DATABASE = 'databaseName';
     const IDP_TABLE_NAME = 'identityProvidersTableName';
     const SP_TABLE_NAME = 'serviceProvidersTableName' ;
+    const ENCRYPTION = 'encryption';
+    const SSL_CA = 'ssl_ca';
+    const SSL_CERT = 'ssl_cert_path';
+    const SSL_KEY = 'ssl_key_path';
+    const SSL_CA_PATH = 'ssl_ca_path';
 
 
 
@@ -26,17 +38,38 @@ class databaseConnector
     {
         $conf = SimpleSAML_Configuration::getConfig(self::CONFIG_FILE_NAME);
         $this->serverName = $conf->getString(self::SERVER);
+        $this->port = $conf->getInteger(self::PORT, null);
         $this->username = $conf->getString(self::USER);
         $this->password = $conf->getString(self::PASSWORD);
         $this->databaseName = $conf->getString(self::DATABASE);
         $this->identityProvidersTableName = $conf->getString(self::IDP_TABLE_NAME);
         $this->serviceProvidersTableName = $conf->getString(self::SP_TABLE_NAME);
+        $this->encryption = $conf->getBoolean(self::ENCRYPTION);
+        $this->sslCA = $conf->getString(self::SSL_CA);
+        $this->sslCert = $conf->getString(self::SSL_CERT);
+        $this->sslKey = $conf->getString(self::SSL_KEY);
+        $this->sslCAPath = $conf->getString(self::SSL_CA_PATH);
     }
 
     public function getConnection()
     {
-        $conn = NULL;
-        $conn = new mysqli($this->serverName, $this->username, $this->password, $this->databaseName);
+        $conn = mysqli_init();
+        if ($this->encryption ===true){
+            SimpleSAML_Logger::debug("Getting connection with encryption.");
+            mysqli_ssl_set($conn, $this->sslKey,$this->sslCert, $this->sslCA, $this->sslCAPath, null);
+            if ($this->port === null){
+                mysqli_real_connect($conn, $this->serverName, $this->username, $this->password, $this->databaseName);
+            } else{
+                mysqli_real_connect($conn, $this->serverName, $this->username, $this->password, $this->databaseName, $this->port );
+            }
+        }
+        else{
+            if ($this->port === null){
+                mysqli_real_connect($conn, $this->serverName, $this->username, $this->password, $this->databaseName);
+            } else{
+                mysqli_real_connect($conn, $this->serverName, $this->username, $this->password, $this->databaseName, $this->port );
+            }
+        }
         return $conn;
     }
 
