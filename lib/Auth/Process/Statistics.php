a5e772e9f8d67524d373898f88288062ef6e4bbe
Dominik Baránek <0Baranek.dominik0@gmail.com>
2020-04-24 14:13:57 +0200

    Removed logging info about login

--- lib/Auth/Process/Statistics.php
+++ lib/Auth/Process/Statistics.php
@@ -24,29 +24,5 @@ class Statistics extends ProcessingFilter
         $dateTime = new DateTime();
         $dbCmd = new DatabaseCommand();
         $dbCmd->insertLogin($request, $dateTime);
-        $spEntityId = $request['SPMetadata']['entityid'];
-
-        $eduPersonUniqueId = '';
-        $sourceIdPEppn = '';
-        $sourceIdPEntityId = '';
-
-        if (isset($request['Attributes']['eduPersonUniqueId'][0])) {
-            $eduPersonUniqueId = $request['Attributes']['eduPersonUniqueId'][0];
-        }
-        if (isset($request['Attributes']['sourceIdPEppn'][0])) {
-            $sourceIdPEppn = $request['Attributes']['sourceIdPEppn'][0];
-        }
-        if (isset($request['Attributes']['sourceIdPEntityID'][0])) {
-            $sourceIdPEntityId = $request['Attributes']['sourceIdPEntityID'][0];
-        }
-
-        if (isset($request['perun']['user'])) {
-            $user = $request['perun']['user'];
-            Logger::notice('UserId: ' . $user->getId() . ', identity: ' . $eduPersonUniqueId . ', service: '
-                . $spEntityId . ', external identity: ' . $sourceIdPEppn . ' from ' . $sourceIdPEntityId);
-        } else {
-            Logger::notice('User identity: ' . $eduPersonUniqueId . ', service: ' . $spEntityId .
-                ', external identity: ' . $sourceIdPEppn . ' from ' . $sourceIdPEntityId);
-        }
     }
 }
